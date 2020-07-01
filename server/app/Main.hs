33c9619485265939816280ef99b562bbdc173b2f
Sean Hess <seanhess@gmail.com>
2020-07-01 13:21:05 -0600

    serverless invoke local working

---
+++ server/app/Main.hs
@@ -0,0 +1,11 @@
+import qualified Data.Aeson as Aeson
+
+import AWSLambda
+
+main = lambdaMain handler
+
+handler :: Aeson.Value -> IO Int
+handler evt = do
+  putStrLn "This should go to logs"
+  print evt
+  pure 8
